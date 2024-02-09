9a37ce2fc308462d0f8e23a43d6b193a754b5053
peterelia22 <peterelia914@gmail.com>
2024-02-09 16:22:55 +0200

    splash view body

---
+++ lib/features/splash/presentation/view/splash.dart
@@ -0,0 +1,11 @@
+import 'package:bookly/features/splash/presentation/view/widgets/splash_view_bodr.dart';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SplashViewBody();
+  }
+}
