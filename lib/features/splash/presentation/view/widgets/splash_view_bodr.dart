c5922c7558a45eee0b4259d982f18fd95ea56a47
peterelia22 <peterelia914@gmail.com>
2024-02-11 17:16:00 +0200

    add animation

--- lib/features/splash/presentation/view/widgets/splash_view_bodr.dart
+++ lib/features/splash/presentation/view/widgets/splash_view_bodr.dart
@@ -1,17 +1,51 @@
 import 'package:bookly/constants.dart';
+import 'package:bookly/features/splash/presentation/view/widgets/widgets.dart';
 
 // Corrected import statement
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
-  const SplashViewBody({Key? key}) : super(key: key); // Corrected constructor
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({Key? key}) : super(key: key);
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  // Corrected constructor
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 20), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [Image.asset(Assets.logo)], // Using the correct asset path
+      children: [
+        Image.asset(Assets.logo),
+        const SizedBox(
+          height: 15,
+        ),
+        SlidingAnimation(slidingAnimation: slidingAnimation),
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class BooklyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData().copyWith(scaffoldBackgroundColor: primaryColor),
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: primaryColor),
       home: const SplashView(),
     );
   }
